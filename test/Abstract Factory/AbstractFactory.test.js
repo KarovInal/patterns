88052593a85cfc94df847a4fa86c6e83c3928566
София <sofia@192.168.1.5>
2017-11-13 13:16:23 -0800

    update test

--- test/Abstract Factory/AbstractFactory.test.js
+++ test/Abstract Factory/AbstractFactory.test.js
@@ -1,14 +1,24 @@
 import ButtonFactory from '../../patterns/Abstract Factory';
 
-test('Абстрактная фабрика.', () => {
+
+describe('Абстрактная фабрика', () => {
   let CreatorIosButton = new ButtonFactory('ios');
   let CreatorWinButton = new ButtonFactory('win');
   let CreatorButton = new ButtonFactory();
+
   let iosButton = CreatorIosButton.create();
   let winButton = CreatorWinButton.create();
-  let Button = CreatorButton.create();
-  
-  expect(iosButton).toEqual({ title: 'IOS button' })
-  expect(winButton).toEqual({ title: 'WIN button' })
-  expect(Button).toEqual({ title: 'Default button' })
-})
+  let button = CreatorButton.create();
+
+  test('Кнопка по умолчанию', () => {
+    expect(button).toEqual({ title: 'Default button' })
+  });
+
+  test('ios кнопка', () => {
+    expect(iosButton).toEqual({ title: 'IOS button' });
+  })
+
+  test('win кнопка', () => {
+    expect(winButton).toEqual({ title: 'WIN button' });
+  })
+});
